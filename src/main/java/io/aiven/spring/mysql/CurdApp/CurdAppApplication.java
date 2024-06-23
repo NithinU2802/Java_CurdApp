dd6f5864e93413bd61662e3f3212f44333f47e2c
NithinU2802 <nithinu2810@gmail.com>
2024-06-23 11:40:26 +0530

    Curd Application with Aiven

---
+++ src/main/java/io/aiven/spring/mysql/CurdApp/CurdAppApplication.java
@@ -0,0 +1,14 @@
+package io.aiven.spring.mysql.CurdApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurdAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(CurdAppApplication.class, args);
+	}
+
+}
